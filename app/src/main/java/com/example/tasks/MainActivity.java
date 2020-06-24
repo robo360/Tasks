b9cf5b2b1a8b7c546709431e104797d061ea679c
Roberto Berwa <rberwa@fb.com>
2020-06-23 21:03:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tasks/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    //declaring widgets, learning point: android studio imports declared materials
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //finding the widgets defined in xml layout.
+        //referring to a widget uses the form R.id.id_widget whereas in XML @+id.id_widget
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+        items = new ArrayList<>();
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
+            @Override
+            public void onItemLongClicked(int position) {
+                items.remove(position);
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(MainActivity.this, "Item removed", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(  this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String todoItem = etItem.getText().toString();
+                items.add(todoItem);
+                itemsAdapter.notifyItemInserted(items.size() - 1 );
+                etItem.setText("");
+                Toast.makeText(MainActivity.this, "Item added", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+    }
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items", e);
+            items = new ArrayList<>();
+        }
+    }
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items", e);
+        }
+    }
+}
